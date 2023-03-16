3dcbcb7bd76a4537359c117c44663f2665049fa7
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-03-16 21:02:03 +0530

    Add files via upload

---
+++ ReverseEqualParts.java
@@ -0,0 +1,9 @@
+package Today15Mar;
+import java.util.Scanner;
+
+public class ReverseEqualParts {
+    public static void main(String[] args) {
+    	StringBuffer s=new StringBuffer("Just Do It"); //take string buffer 
+		s.reverse();
+		System.out.println("After reversing :"+s); //print the output after reverse
+	}}

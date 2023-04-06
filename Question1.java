54e7cf2504f35a96d9a3809adbdd51ca465695a6
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-04-06 20:37:37 +0530

    Add files via upload

--- Question1.java
+++ Question1.java
@@ -1,23 +1,47 @@
-package Today23Mar;
-
-import java.util.*;
+package Today6Apr;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Question1 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x, y;
-        try {
-            x = sc.nextInt();
-            y = sc.nextInt();
-            if (y == 0) {
-                throw new ArithmeticException("/ by zero");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your choice: 1 for store data, 2 for print data");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Enter data to store in file:");
+            scanner.nextLine(); // consume newline left-over
+
+            String data = scanner.nextLine();
+            try {
+                File file = new File("data.txt");
+                FileWriter writer = new FileWriter(file);
+                writer.write(data);
+                writer.close();
+                System.out.println("Data successfully stored in file!");
+            } catch (IOException e) {
+                System.out.println("Error storing data in file: " + e.getMessage());
             }
-            int result = x / y;
-            System.out.println(result);
-        } catch (InputMismatchException e) {
-            System.out.println("Input Mismatch Exception");
-        } catch (ArithmeticException e) {
-            System.out.println("Exception caught: " + e.getMessage());
+        } else if (choice == 2) {
+            try {
+                File file = new File("data.txt");
+                Scanner fileScanner = new Scanner(file);
+                while (fileScanner.hasNextLine()) {
+                    String line = fileScanner.nextLine();
+                    System.out.println(line);
+                }
+                fileScanner.close();
+            } catch (IOException e) {
+                System.out.println("Error reading data from file: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Invalid choice!");
         }
+
+        scanner.close();
     }
 }

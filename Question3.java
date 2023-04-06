cdc94aabe71174faa337450c3859d005bc71af6b
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-04-06 20:38:12 +0530

    Add files via upload

--- Question3.java
+++ Question3.java
@@ -1,24 +1,55 @@
-package Today23Mar;
+package Today6Apr;
+import java.io.*;
 
-import java.util.Scanner;
+class Person implements Serializable {
+    private String name;
+    private transient int age; // Mark age as transient to exclude it from serialization
 
-public class Question3 {
-    public static int addTwoPositive() {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        if (a <= 0 || b <= 0) {
-            throw new ArithmeticException("Non-positive integer sent");
-        }
-        return a + b;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Question3 {
+
     public static void main(String[] args) {
+        Person person = new Person("John Doe", 30);
+
         try {
-            int sum = addTwoPositive();
-            System.out.println(sum);
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
+            FileOutputStream fileOutputStream = new FileOutputStream("person.ser");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            // Write object to file
+            objectOutputStream.writeObject(person);
+
+            objectOutputStream.close();
+            fileOutputStream.close();
+
+            System.out.println("Person object has been serialized.");
+
+            FileInputStream fileInputStream = new FileInputStream("person.ser");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            // Read object from file
+            Person deserializedPerson = (Person) objectInputStream.readObject();
+
+            objectInputStream.close();
+            fileInputStream.close();
+
+            System.out.println("Deserialized person object:");
+            System.out.println("Name: " + deserializedPerson.getName());
+            System.out.println("Age: " + deserializedPerson.getAge()); // Age will be 0 since it was marked as transient
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
